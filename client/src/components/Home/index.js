abe11d4d543344e8c0d9e1cd1582d29aacb6ebd6
henry lee <djteski@gmail.com>
2022-02-07 22:38:09 -0600

    front back connected

--- client/src/components/Home/index.js
+++ client/src/components/Home/index.js
@@ -1,8 +1,17 @@
 import React from 'react'
+import axios from 'axios'
 
 const Home = () => {
+    const handleClick = async () => {
+        const response = await axios.get('http://localhost:8000')
+        
+        console.log(response)
+    }
     return (
+        <>
         <div>Hello from Home</div>
+        <button onClick={handleClick}>connect</button>
+        </>
     )
 }
 
--- server/server.js
+++ server/server.js
@@ -1,6 +1,17 @@
 const express = require('express');
 const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
 const PORT = 8000;
+
+app.use( bodyParser.json() )
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors())
+
+app.get('/', (req, res) => {
+  console.log('request received')
+  return res.status(200).send('hello from server')
+})
 
 app.listen(PORT, () => {
   console.log(`Server is listening on port ${PORT}`);
